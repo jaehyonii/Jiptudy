b6cce8f80c6e286617d91b6492432352e7c37c3c
Jaehyun Kim <kk2007pp@gmaio.com>
2024-09-22 19:49:25 +0900

    BOJ 2644 Dijkstra

--- KJH/BOJ/2644.py
+++ KJH/BOJ/2644.py
@@ -15,27 +15,29 @@ for _ in range(k):
 distance = [INF] * (n + 1)
 
 def dijkstra(start):
-	heapq.heappush(hq, (0, start))
-	distance[start] = 0
-	
 	hq = []
+	# 시작 노드로의 최단 경로는 0
+	distance[start] = 0
+	heapq.heappush(hq, (0, start)) # (거리, 토드)
+	
 	while hq:
+		# 가장 거리가 짧은 노드 정보 꺼내기
 		dist, now = heapq.heappop(hq)
+		# 이미 처리된 노드라면 무시
 		if distance[now] < dist:
 			continue
 		
+		# 현재 노드와 인접한 노드들 확인
 		for next in graph[now]:
 			cost = dist + 1
+			# 현재 노드를 거쳐서 가는 경로가 더 짧다면
 			if cost < distance[next]:
 				distance[next] = cost
-				heapq.heappush(hq, (cost, a))
+				heapq.heappush(hq, (cost, next))
 
 dijkstra(a)
 
-if distance[b] < INF:
+if distance[b] != INF:
 	print(distance[b])
 else:
 	print(-1)
-
-
-
