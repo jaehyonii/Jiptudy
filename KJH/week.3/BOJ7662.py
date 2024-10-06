542c099b9356303cb0c0aae4f05c1a8859b61d52
Jaehyun Kim <kk2007pp@gmaio.com>
2024-10-06 21:44:47 +0900

    add comment

--- KJH/week.3/BOJ7662.py
+++ KJH/week.3/BOJ7662.py
@@ -13,7 +13,7 @@ for _ in range(t):
 	k = int(input())
 	min_heap = []
 	max_heap = []
-	nums = dict()
+	nums = dict() # 현재 큐에 각 숫자가 몇 개 들어있는지 나타냄 {숫자: 개수}
 	for _ in range(k):
 		c, x = input().split()
 		x = int(x)
@@ -21,35 +21,35 @@ for _ in range(t):
 		if c == 'I':
 			hq.heappush(min_heap, x)
 			hq.heappush(max_heap, -x)
-			if x in nums:
-				nums[x] += 1
-			else:
-				nums[x] = 1
+			if x in nums: # 이미 존재하는 수면
+				nums[x] += 1 # 개수 증가 기록
+			else: # 큐에 없는 수가 들어오면
+				nums[x] = 1 # 새로운 수 추가 기록
 
 		elif c == 'D' and not isEmpty(nums):
 			value = 0
-			if x == 1:
+			if x == 1: # 최대값 삭제
 				value = -hq.heappop(max_heap)
-				while value not in nums:
+				while value not in nums: # 존재하지 않는 수라면 한 번 더 pop
 					value = -hq.heappop(max_heap)
-			elif x == -1:
+			elif x == -1: # 최소값 삭제
 				value = hq.heappop(min_heap)
-				while value not in nums:
+				while value not in nums: # 존재하지 않는 수라면 한 번 더 pop
 					value = hq.heappop(min_heap)
-			nums[value] -= 1
-			if nums[value] == 0:
+			nums[value] -= 1 # 삭제된 수 개수 감소 기록
+			if nums[value] == 0: # 삭제된 결과 큐에 더 이상 존재하지 않게되면 기록에서 삭제
 				nums.pop(value)
 	
 	if isEmpty(nums):
 		print("EMPTY")
 	else:
 		min_v = min_heap[0]
-		while min_v not in nums:
+		while min_v not in nums: # 존재하지 않는 수라면 한 번 더 pop
 			hq.heappop(min_heap)
 			min_v = min_heap[0]
 			
 		max_v = -max_heap[0]
-		while max_v not in nums:
+		while max_v not in nums: # 존재하지 않는 수라면 한 번 더 pop
 			hq.heappop(max_heap)
 			max_v = -max_heap[0]
 
