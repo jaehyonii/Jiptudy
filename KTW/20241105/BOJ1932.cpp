c7cc8fe02f925cbb60d3ef36ea2bc9d40f86c3cc
taewanki <kimtawann@naver.com>
2024-11-05 00:44:14 +0900

    BOJ 1932 DP

--- KTW/20241105/BOJ1932.cpp
+++ KTW/20241105/BOJ1932.cpp
@@ -16,16 +16,16 @@ int main() {
             if (i == j && i == 0)
                 continue;
             if (j == 0)
-                tree[j][i] = tree[i][j] + tree[j][i-1];
+                tree[i][j] += tree[i-1][j];
             else if (i != 0 && i == j)
                 tree[i][j] += tree[i-1][j-1];
             else
-                tree[j][i] = max(tree[j][i-1], tree[j-1][i-1]) + tree[i][j];
+                tree[i][j] += max(tree[i-1][j], tree[i-1][j-1]);
         }
     }
     maxx = 0;
-    for (int i = 0; i < n; i++)
-        maxx = max(maxx, tree[i][n-1]);
+    for (int j = 0; j < n; j++)
+        maxx = max(maxx, tree[n-1][j]);
     cout << maxx;
     return 0;
 }
