a94c2526b95aae673c1f434a23d6d884237a9dc2
Jaehyun Kim <kk2007pp@gmaio.com>
2024-11-05 03:41:46 +0900

    BOJ 2670 DP

---
+++ KJH/week.4/BOJ2670.py
@@ -0,0 +1,15 @@
+import math
+
+n = int(input())
+array = []
+
+for _ in range(n):
+	array.append(float(input()))
+
+thisMul = array[0]
+maxMul = array[0]
+for k in range(1, n):
+	thisMul = max(thisMul * array[k], array[k])
+	maxMul = max(thisMul, maxMul)
+
+print("%.3f"%maxMul)
