7ecb9c1febcd2a6256fc30064a58e8ce61058797
Jaehyun Kim <kk2007pp@gmaio.com>
2024-11-08 21:41:06 +0900

    BOJ 1463 DP

--- KJH/week.5/1463.py
+++ KJH/week.5/1463.py
@@ -1,7 +1,16 @@
 n = int(input())
-dp = [1e9] * (n + 1)
+dp = [1e9] * 1000001
 
-def solution(num):
-	if dp[num] != 1e9:
-		return dp[num]
-	a = 
+dp[0] = 0
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, n+1):
+	n1 = dp[i - 1]
+	n2 = dp[i // 2] if i % 2 == 0 else 1e9
+	n3 = dp[i // 3] if i % 3 == 0 else 1e9
+	
+	dp[i] = min(n1, n2, n3) + 1
+
+print(dp[n])
